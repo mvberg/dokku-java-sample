a41189a1cea990755bd358a7698ce947237f8cbf
Mike <mvberg@gmail.com>
2013-12-02 19:19:12 -0600

    added

---
+++ src/main/java/com/eba/one/test/Main.java
@@ -0,0 +1,30 @@
+package com.eba.one.test;
+
+public class Main {
+
+	public Main m;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+		new Main();
+	}
+
+	public Main() {
+		while (!Thread.interrupted()) {
+
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				return;
+			}
+
+		}
+
+		System.out.println("Goodbye world!");
+	}
+
+}
